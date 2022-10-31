2f3c331983e0d8f678606e6a13e76a858264bd2e
Josué Teobaldo dos Santos <josueteobaldo220@hotmail.com>
2022-10-31 13:03:25 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -8,10 +8,15 @@ var textoImage3 = document.getElementById('imageLabel3')
 var entradaImagem1 = document.getElementById('imageEntry1')
 var entradaImagem2 = document.getElementById('imageEntry2')
 var entradaImagem3 = document.getElementById('imageEntry3')
+var boxImage1 = document.getElementById('imgStyle1')
 
 entradaImagem1.addEventListener('mouseenter', ()=>{
-    tituloImage1.innerHTML = 'Titulo alguma coisa'
+    tituloImage1.innerHTML = "Lorem Lorem Lorem Lorem"
+    boxImage1.style.boxShadow = '2px 2px 2px 2px rgba(0, 0, 0, 0.370)'
+    boxImage1.style.backgroundColor = 'rgba(0, 0, 0, 0.750)'
 })
 entradaImagem1.addEventListener('mouseleave', ()=>{
-    tituloImage1.innerHTML = 'Titulo 1'
+    tituloImage1.innerHTML = "Titulo 2"
+    boxImage1.style.boxShadow = ''
+    boxImage1.style.backgroundColor = 'rgba(0, 0, 0, 0.400)'
 })
